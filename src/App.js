81925de0582f65b65084d1fe6b4da4ce2158959c
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-02-27 11:15:25 +0530

    added source code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { HashRouter, Route } from "react-router-dom";
+
+import "./App.css";
+import "../node_modules/video-react/dist/video-react.css";
+import Display from "./components/Display";
+
+const App = () => {
+  return (
+    <div className="container">
+      <HashRouter>
+        <Route path="/" component={Display} />
+      </HashRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Display.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {
+  Player,
+  ControlBar,
+  PlaybackRateMenuButton,
+  BigPlayButton
+} from "video-react";
+
+import Nav from "./Nav";
+import CustomForwardReplayControl from "./CustomForwardReplayControl";
+import CustomPlayToggle from "./CustomPlayToggle";
+import CustomProgressControl from "./CustomProgressControl";
+import CustomBigPlayButton from "./CustomBigPlayButton";
+const CustomForwardControl = CustomForwardReplayControl("forward");
+const CustomReplayControl = CustomForwardReplayControl("replay");
+
+class Display extends Component {
+  render() {
+    return (
+      <div>
+        <Nav />
+        <h3 className="text-center"> Movie Recommender </h3>
+        <hr />
+
+        <div className="col-sm-12">
+          <Player
+            disableDefaultControls
+            src="http://res.cloudinary.com/guptautkarsh/video/upload/v1519069513/Binary_tree_traversal_-_breadth-first_and_depth-first_strategies.mp4"
+          >
+            <CustomBigPlayButton position="center" />
+            <ControlBar autoHide>
+              <CustomReplayControl seconds={5} order={2.1} />
+              <CustomForwardControl seconds={5} order={3.1} />
+              <CustomProgressControl order={6} />
+              <PlaybackRateMenuButton
+                rates={[5, 3, 1.5, 1, 0.5, 0.1]}
+                order={7.1}
+              />
+              <CustomPlayToggle />
+            </ControlBar>
+          </Player>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Display;
