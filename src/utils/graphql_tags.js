be3a98b91e228b17ee1556a5ece1366b3695e0d7
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-03-25 17:53:12 +0530

    Added Sigin and Signup gql tags

--- src/utils/graphql_tags.js
+++ src/utils/graphql_tags.js
@@ -1,9 +1,25 @@
-import gql from "graphql-tag";
+import gql from 'graphql-tag';
 
 export const LOG_INTERACTION = gql`
   mutation LogUserInteractionMutation($time: String!, $type: String!) {
     logUserInteraction(time: $time, type: $type) {
       interactionType
+    }
+  }
+`;
+
+export const SIGNUP_MUTATION = gql`
+  mutation CreateUserMutation($authProvider: AuthData!, $name: String!) {
+    createUser(authProvider: $authProvider, name: $name) {
+      id
+    }
+  }
+`;
+
+export const SIGNIN_MUTATION = gql`
+  mutation SigninUserMutation($auth: AuthData) {
+    signinUser(auth: $auth) {
+      token
     }
   }
 `;
