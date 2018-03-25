0bdb5016171bfdf474d3de6b037cc47296f856d8
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-03-25 17:51:57 +0530

    Allowed Access-Control-Allow-Credentials header in CORS

--- recflix-backend/src/main/java/com/recflix/filters/CORSFilter.java
+++ recflix-backend/src/main/java/com/recflix/filters/CORSFilter.java
@@ -42,6 +42,7 @@ public class CORSFilter implements Filter {
 
         // Authorize (allow) all domains to consume the content
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Origin", "*");
+        ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Credentials", "true");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Methods",
                 "GET, OPTIONS, HEAD, PUT, POST");
         ((HttpServletResponse) servletResponse).addHeader("Access-Control-Allow-Headers",
