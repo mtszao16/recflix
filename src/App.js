0fe7e7f640ab8af4403077fdbb42ff7fdb04553d
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-03-29 03:03:47 +0530

    Added check for jwt auth token

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import '../node_modules/video-react/dist/video-react.css';
 import Display from './components/Display';
 import Header from './components/Header';
-import Login from './components/Login';
+import Auth from './components/Auth';
+import Landing from './components/Landing';
+import NoMatch from './components/NoMatch';
 
-const App = props => (
-  <React.Fragment>
-    <Header />
-    <div className="container">
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/signup" component={SignUp} /> */}
-        <Route path="/" component={Display} />
-      </Switch>
-    </div>
-  </React.Fragment>
-);
+import { AUTH_TOKEN } from './utils/constants';
+
+const App = props => {
+  const authToken = localStorage.getItem(AUTH_TOKEN);
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="main-bg">
+        <img alt="Landing Page" src="./assets/images/movie-audience-alt2.jpg" />
+      </div>
+      {authToken ? (
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" render={() => <Redirect to="/" />} />
+          <Route exact path="/movie" component={Display} />
+          <Route component={NoMatch} />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route exact path="/" render={() => <Redirect to="/login" />} />
+          <Route exact path="/login" component={Auth} />
+          <Route component={NoMatch} />
+        </Switch>
+      )}
+    </React.Fragment>
+  );
+};
 
 export default App;
