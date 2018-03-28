2b225649c4170f3b082e062747b072872f5a4e38
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-03-29 03:03:17 +0530

    Minor refactor

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import {
   Player,
   ControlBar,
@@ -17,26 +16,28 @@ const CustomReplayControl = CustomForwardReplayControl('replay');
 class Display extends Component {
   render() {
     return (
-      <div>
-        <div className="col-sm-12">
-          <Player
-            disableDefaultControls
-            src="http://res.cloudinary.com/guptautkarsh/video/upload/v1519069513/Binary_tree_traversal_-_breadth-first_and_depth-first_strategies.mp4"
-          >
-            <CustomBigPlayButton position="center" />
-            <ControlBar autoHide>
-              <CustomReplayControl seconds={5} order={2.1} />
-              <CustomForwardControl seconds={5} order={3.1} />
-              <CustomProgressControl order={6} />
-              <PlaybackRateMenuButton
-                rates={[5, 3, 1.5, 1, 0.5, 0.1]}
-                order={7.1}
-              />
-              <CustomPlayToggle />
-            </ControlBar>
-          </Player>
+      <React.Fragment>
+        <div className="video-player">
+          <div className="col-sm-12">
+            <Player
+              disableDefaultControls
+              src="http://res.cloudinary.com/guptautkarsh/video/upload/v1519069513/Binary_tree_traversal_-_breadth-first_and_depth-first_strategies.mp4"
+            >
+              <CustomBigPlayButton position="center" />
+              <ControlBar autoHide>
+                <CustomReplayControl seconds={5} order={2.1} />
+                <CustomForwardControl seconds={5} order={3.1} />
+                <CustomProgressControl order={6} />
+                <PlaybackRateMenuButton
+                  rates={[5, 3, 1.5, 1, 0.5, 0.1]}
+                  order={7.1}
+                />
+                <CustomPlayToggle />
+              </ControlBar>
+            </Player>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
