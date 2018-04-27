eda11b5e013045c8e4907a458f4f74c31352f1a6
UtkarshGupta-CS <utgupta1997@gmail.com>
2018-04-27 06:42:04 +0530

    Fixed the logic for feedback mutation and updation to db

--- recflix-backend/src/main/java/com/recflix/app/FeedbackRepository.java
+++ recflix-backend/src/main/java/com/recflix/app/FeedbackRepository.java
@@ -1,6 +1,8 @@
 package com.recflix.app;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Updates;
 
 import org.bson.Document;
 import org.bson.types.ObjectId;
@@ -10,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.and;
 
 /**
  * Manages Feedback persistence
@@ -36,22 +39,33 @@ public class FeedbackRepository {
     }
 
     public Feedback saveFeedback(Feedback feedback) {
+        FindIterable<Document> docs = feedbacks
+                .find(and(eq("userId", feedback.getUserId()), eq("movieId", feedback.getMovieId())));
+
         Document doc = new Document();
-        doc.append("rating", feedback.getRating());
-        doc.append("type", feedback.getType());
-        doc.append("userId", feedback.getUserId());
-        doc.append("movieId", feedback.getMovieId());
-        doc.append("createdAt", Scalars.dateTime.getCoercing().serialize(feedback.getCreatedAt()));
-        feedbacks.insertOne(doc);
+        Integer docsCount = 0;
+        for (Document el : docs) {
+            docsCount += 1;
+        }
+
+        if (docsCount < 1) {
+            doc.append("rating", feedback.getRating());
+            doc.append("type", feedback.getType());
+            doc.append("userId", feedback.getUserId());
+            doc.append("movieId", feedback.getMovieId());
+            doc.append("createdAt", Scalars.dateTime.getCoercing().serialize(feedback.getCreatedAt()));
+            feedbacks.insertOne(doc);
+        } else {
+            feedbacks.updateOne(and(eq("userId", feedback.getUserId()), eq("movieId", feedback.getMovieId())),
+                    Updates.set("rating", feedback.getRating()));
+        }
 
         return new Feedback(doc.get("_id").toString(), doc.getInteger("rating"), doc.getString("type"),
-                ZonedDateTime.parse(doc.getString("createdAt")), doc.getString("userId"),
-                doc.getString("movieId"));
+                ZonedDateTime.parse(doc.getString("createdAt")), doc.getString("userId"), doc.getString("movieId"));
     }
 
     private Feedback feedback(Document doc) {
         return new Feedback(doc.get("_id").toString(), doc.getInteger("rating"), doc.getString("type"),
-                ZonedDateTime.parse(doc.getString("createdAt")), doc.getString("userId"),
-                doc.getString("movieId"));
+                ZonedDateTime.parse(doc.getString("createdAt")), doc.getString("userId"), doc.getString("movieId"));
     }
 }
